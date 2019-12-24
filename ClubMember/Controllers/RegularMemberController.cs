23bd54efd0dcb759134e28f2ff4f689a46e9107f
cgarcia86 <58064023+cgarcia86@users.noreply.github.com>
2019-12-24 04:50:28 -0400

    Edit and Delete user from Data Base

--- ClubMember/Controllers/RegularMemberController.cs
+++ ClubMember/Controllers/RegularMemberController.cs
@@ -15,6 +15,12 @@ namespace WebDesignTest.Controllers
         ObjectCache cacheSlot = MemoryCache.Default;
         List<RegularMember> listRegMembers;
 
+        SqlConnection DbConnect = new SqlConnection();
+        SqlCommand DbCommand = new SqlCommand();
+        SqlDataReader DbReader;
+
+        
+
         public RegularMemberController()
         {
             listRegMembers = cacheSlot["listRegMembers"] as List<RegularMember>;
@@ -31,23 +37,14 @@ namespace WebDesignTest.Controllers
         }
 
         //Get User List from Data Base
-        public void GetUserList()
+        public void LoadUserListfromDB()
         {
-            
-            SqlConnection DbConnect = new SqlConnection();
-            SqlCommand DbCommand = new SqlCommand();
-            SqlDataReader DbReader;
-
             DbConnect.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet-ClubMember-20191219041643;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
             DbConnect.Open();
-
-           
             DbCommand.Connection = DbConnect;
-            DbCommand.CommandText = "Select FirstName, LastName from dbo.AspNetUsers";
+            DbCommand.CommandText = "Select Id, FirstName, LastName from dbo.AspNetUsers";
             DbReader = DbCommand.ExecuteReader();
-
-           //var result =  DbReader.GetValues();
 
             if(DbReader.HasRows)
             {
@@ -56,27 +53,67 @@ namespace WebDesignTest.Controllers
                 {
                     RegularMember member = new RegularMember();
 
-                    member.firstName = DbReader.GetString(0);
-                    member.lastName = DbReader.GetString(1);
+                    member.ID = DbReader.GetString(0);
+                    member.firstName = DbReader.GetString(1);
+                    member.lastName = DbReader.GetString(2);
 
                     //Saver member Info to Cache
                     listRegMembers.Add(member);
                     cacheSlot["listRegMembers"] = listRegMembers;
-
                 }
                 
             }
+            //else
+            //{
+            //    RedirectToAction("Error");
+            //    Console.WriteLine("Error");
+            //}
+        }
+
+        public bool UpdateMemberfromDB(RegularMember member, string id)
+        {
+            DbConnect.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet-ClubMember-20191219041643;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            DbConnect.Open();
+            DbCommand.Connection = DbConnect;
+            DbCommand.CommandText = "Update dbo.AspNetUsers " +
+                "                    set FirstName = '" + member.firstName +
+                                     "', LastName = '"+ member.lastName +"' where Id = '"+id +"';";
+            DbReader = DbCommand.ExecuteReader();
+
+            if (DbReader.Read())
+            {
+                return (true);
+            }
             else
             {
-                RedirectToAction("Error");
-                Console.WriteLine("Error");
+                return (false);
+            }
+        }
+
+        public bool DeleteMemberfromDB(string id)
+        {
+            DbConnect.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet-ClubMember-20191219041643;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            DbConnect.Open();
+            DbCommand.Connection = DbConnect;
+            DbCommand.CommandText = "Delete from dbo.AspNetUsers where Id = '" + id + "';";
+            DbReader = DbCommand.ExecuteReader();
+
+            if (DbReader.Read())
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
             }
         }
         // GET: RegularMember
         public ActionResult Index()
         {
-            if(cacheSlot["listRegMembers"] == null)
-                GetUserList();
+            if (cacheSlot["listRegMembers"] == null)
+                LoadUserListfromDB();
 
             return View(listRegMembers);
 
@@ -113,9 +150,6 @@ namespace WebDesignTest.Controllers
 
         }
 
-
-
-
         public ActionResult EditMemberInfo(string id)
         {
             RegularMember member = listRegMembers.FirstOrDefault(c => c.ID == id);
@@ -142,9 +176,16 @@ namespace WebDesignTest.Controllers
             }
             else
             {
+                //Changes to Cache
                 memberToEdit.firstName = member.firstName;
                 memberToEdit.lastName = member.lastName;
                 SavecacheSlot();
+
+                //Changes to DB
+                if (!UpdateMemberfromDB(member, id))
+                {
+                    //return HttpNotFound();
+                }
 
                 return RedirectToAction("Index");
             }
@@ -178,7 +219,14 @@ namespace WebDesignTest.Controllers
             }
             else
             {
+                //Changes to Cache
                 listRegMembers.Remove(member);
+
+                //Changes to DB
+                if (!DeleteMemberfromDB(id))
+                {
+                    //return HttpNotFound();
+                }
                 return RedirectToAction("Index");
             }
         }
